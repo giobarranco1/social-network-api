cc576f3725da89b72c0fc4cbb4a9d97af056c4cf
gio_barranco <sandiego_94@hotmail.com>
2022-03-14 02:17:00 -0600

    thought model updated

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,40 @@
 const {Schema, model} = require('mongoose');
+const dateFormat = require("../utils/dateFormat");
+const reactionSchema = require("./Reaction");
 
 //Schema to create a thought model
 const thoughtSchema = new Schema(
     {
         thoughtText: {
             //String
+            type: String,
             //Required
+            required: true,
             //Must be between 1 and 280 characters
+            minlength: 1,
+            maxlength: 280,
         },
         createdAt: {
             //Date
+            type: Date,
             //Set default value to the current timestamp
+            default: Date.now,
             //Use a getter method to format the timestamp on query
+            get: (timestamp) => dateFormat(timestamp),
         },
         username: {
             //String
+            type: String,
             //Required
+            required: true,
         },
-        reaction: {
-            //Array of nested doc created with the reactionSchema 
-        },
+        //Array of nested doc created with the reactionSchema
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
             virtuals: true,
+            getters: true,
         },
     }
 );
